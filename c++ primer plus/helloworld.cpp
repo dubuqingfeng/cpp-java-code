f547be740d36f7df42eb7e819811a326940da776
dubuqingfeng <1135326346@qq.com>
2014-11-18 11:55:09 +1000

    push

--- c++ primer plus/helloworld.cpp
+++ c++ primer plus/helloworld.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
-int main(void)
+int main()
 {
-	cout<<"helloworld";
+	int x,y,z;
+	while(cin>>x)
+	{
+		for(y = 0;y < x;y++)
+		cout<<y<<" "<<x-y<<endl;
+	}
 	return 0;
 }
